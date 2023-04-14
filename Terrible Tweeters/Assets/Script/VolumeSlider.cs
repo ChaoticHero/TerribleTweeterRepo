965770d6ef46b11d866fcf6d74a4242f63f35e31
ChaoticHero <Konnor.Montoya2003@gmail.com>
2023-04-14 12:40:27 -0700

    Sound with slider

--- Terrible Tweeters/Assets/Script/VolumeSlider.cs
+++ Terrible Tweeters/Assets/Script/VolumeSlider.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -11,15 +11,32 @@ public class VolumeSlider : MonoBehaviour
     [SerializeField] AudioMixer _mixer;
     [SerializeField] Slider _slider;
     [SerializeField] float _multiplier = 40f;
+    [SerializeField] private Toggle _toggle;
+    private bool _disableToggleEvent;
 
     public void Awake()
     {
         _slider.onValueChanged.AddListener(HandleSliderValueChanged);
+        _toggle.onValueChanged.AddListener(HandleToggleValueChanged);
+    }
+
+    private void HandleToggleValueChanged(bool enableSound)
+    {
+        if (_disableToggleEvent)
+            return;
+
+        if (enableSound)
+            _slider.value = _slider.maxValue;
+        else
+            _slider.value = _slider.minValue;
     }
 
     private void HandleSliderValueChanged(float value)
     {
         _mixer.SetFloat(_volumeParameter, value:Mathf.Log10(value) * _multiplier);
+        _disableToggleEvent = true;
+        _toggle.isOn = _slider.value > _slider.minValue;
+        _disableToggleEvent = false;
     }
     // Start is called before the first frame update
     void Start()
